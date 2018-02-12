e7e92b2ebc8b885a76f8ede08427df26212d393c
hebergutierrez.p@gmail.com <hebergutierrez.p@gmail.com>
2018-02-11 20:09:36 -0600

    Create a Class and Instantation with render

--- src/app.js
+++ src/app.js
@@ -2,6 +2,15 @@ import './styles.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class App extends React.Component {
+  render() {
+    return <div class = "app" >
+      <
+      p > Heber rocks! < /p> < /
+    div > ;
+  }
+}
+
 const root = document.getElementById('app-container');
 
-ReactDOM.render( < p > Hello world! < /p>, root);
+ReactDOM.render( < App / > , root);
