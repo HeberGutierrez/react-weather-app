b7b3285915c2c2ce481fc8b0a6be06790ba49691
hebergutierrez.p@gmail.com <hebergutierrez.p@gmail.com>
2018-02-13 08:50:30 -0600

    add zipcode component

--- src/app.js
+++ src/app.js
@@ -1,15 +1,7 @@
 import './styles.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-class App extends React.Component {
-  render() {
-    return <div class = "app" >
-      <
-      p > Heber rocks! < /p> < /
-    div > ;
-  }
-}
+import App from './components/App';
 
 const root = document.getElementById('app-container');
 
---
+++ src/components/ZipForm.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class ZipForm extends React.Component{
+  render(){
+    return (
+      <div className="zip-form">
+        <form>
+          <label htmlFor="zipcode">Zip Code</label>
+          <input
+            className="form-control"
+            type="input"
+            name="zipcode"/>
+          <button type="submit" className='btn btn-success'>Get the forecast!!</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+
+exports default ZipForm;
