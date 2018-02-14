4aac4947e868ce49c2a8312113e6c7236f78aefd
hebergutierrez.p@gmail.com <hebergutierrez.p@gmail.com>
2018-02-13 18:18:35 -0600

    refactor weather list item

--- src/components/WeatherListItem.js
+++ src/components/WeatherListItem.js
@@ -1,31 +1,17 @@
 import React, { Component } from 'react';
 
-class WeatherListItem extends Component {
-  constructor(props){
-    super(props);
+const onClick = (index, callback) => () => callback(index);
 
-    this.onClick = this.onClick.bind(this);
-  }
+const WeatherListItem = ({ day, index, onDayClicked }) => {
+  const date = new Date(day.dt * 1000);
 
-
-  onClick() {
-    const { onDayClicked, index } = this.props
-
-    onDayClicked(index);
-  }
-
-  render() {
-    const { day } = this.props;
-    const date = new Date(day.dt * 1000);
-
-    return (
-      <div className="weather-list-item" onClick={this.onClick}>
-        <h1>{date.getMonth() + 1} / {date.getDate()}</h1>
-        <h2>{day.temp.min.toFixed(1)}&deg;F &#124; {day.temp.max.toFixed(1)}&deg;F</h2>
-      </div>
-    );
-  }
- }
+  return (
+    <div className="weather-list-item" onClick={onClick(index, onDayClicked)}>
+      <h1>{date.getMonth() + 1} / {date.getDate()}</h1>
+      <h2>{day.temp.min.toFixed(1)}&deg;F &#124; {day.temp.max.toFixed(1)}&deg;F</h2>
+    </div>
+  );
+};
 
 
 
