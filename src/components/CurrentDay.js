ad01f23c02c997cfcedb64cdaf9f630d75d08036
hebergutierrez.p@gmail.com <hebergutierrez.p@gmail.com>
2018-02-13 18:16:07 -0600

    refactor currentday with functional component

--- src/components/CurrentDay.js
+++ src/components/CurrentDay.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
 
-class CurrentDay extends Component {
+const CurrentDay = ({
+  city,
+  day: { temp, weather, pressure, humidity, speed, deg }
+}) => {
+  const w = weather[0];
 
-  render(){
-    const { city, day: { temp, weather, pressure, humidity, speed, deg  }} = this.props;
-    const w = weather[0];
-
-    return (
-      <div className="current-day">
-        <h1 className="day-header">{city.name}</h1>
-        <div className="weather">
+  return (
+    <div className="current-day">
+      <h1 className="day-header">{city.name}</h1>
+      <div className="weather">
           <p>
             <img src={`http://openweathermap.org/img/w/${w.icon}.png`} alt={w.description}/>
             {w.description}
           </p>
         </div>
-      <div className="details felex-parent">
+      <div className="details flex-parent">
         <div className="temperature-breakdown">
           <p>Morning Temperature: {temp.morn}&deg;F</p>
           <p>Day Temperature: {temp.day}&deg;F</p>
@@ -30,7 +30,6 @@ class CurrentDay extends Component {
       </div>
     </div>
   );
- }
-}
+};
 
 export default CurrentDay;
